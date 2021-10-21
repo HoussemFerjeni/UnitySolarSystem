1eedcd600e3a9148d95cfb719bee23c82080cd35
ferjani <houssem.ferjani@esprit.tn>
2021-10-21 16:23:49 +0100

    first

---
+++ Assets/scripts/sunmvt.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sunmvt : MonoBehaviour
+{
+public float Daylength;
+float _rotationspeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _rotationspeed = Time.deltaTime / Daylength;
+        transform.Rotate(0,_rotationspeed,0);
+    }
+}
